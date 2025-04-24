9d1f6e6fa9d6c3d82bfaa5cc81bef3defb2c6a42
Aman-7x <amankhan44899@gmail.com>
2025-04-24 15:32:26 +0530

    Comperision Opr.

---
+++ Basics/04_compersion.js
@@ -0,0 +1,15 @@
+
+// console.log(null==0); here Null is not convert as 0 , loosely couple to undefined
+// console.log(null>=0); here Null is converted to 0 , 
+// console.log(null>0); here Null is not convert as 0 , loosely couple to undefined
+// console.log(null<0);here Null is not convert as 0 , loosely couple to undefined
+// console.log(null<=0);here Null is converted to 0 ,
+// console.log(null==undefined); here Null is not convert as 0 , loosely couple to undefined
+
+// console.log(undefined ==1); 
+// Not recommented 
+
+///===
+// console.log("2"===2); //Strict Checking Also Check Data type
+
+
