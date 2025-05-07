3eb0a0da440b0a0aea71a8618cba058cdb96d6d6
Aman-7x <amankhan44899@gmail.com>
2025-05-07 13:48:47 +0530

    Loops

---
+++ Control_Flow/one.js
@@ -0,0 +1,39 @@
+
+
+// let temperature = 200;
+
+// if(temperature>100){
+//     let user = "Aman";
+//     console.log(`user is ${user}`);
+// }
+
+// output : user is Aman
+
+const a = 1000;
+//  if(a>500) console.log("Tested True") , console.log("Tested 2 True");
+//Avoid this syntax , Not recommented and vey immature code. (Not a Good Practice)
+
+// if(a<500){
+//     console.log("Less than 500");
+    
+// }else if(a<750){
+//     console.log("Less than 750");
+// }else if(a<1200){
+//     console.log("Less than 1200");
+// }
+
+const isLoggedIn = true;
+const isDebit = true;
+
+
+// if(isDebit && isLoggedIn){
+//     console.log("Allow User");
+    
+// }
+if(isDebit || !isLoggedIn){
+    console.log("Allow User");
+    
+}
+
+
+ 
