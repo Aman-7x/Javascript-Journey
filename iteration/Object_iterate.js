033f2021f9ca8a0b96600f137a14891b61ad9fd4
Aman-7x <amankhan44899@gmail.com>
2025-05-07 14:03:30 +0530

    Object_Iterattion

---
+++ iteration/Object_iterate.js
@@ -0,0 +1,12 @@
+const obj={
+     js: 'javascript',
+     cpp:'C++',
+    rb: 'ruby',
+    swift: "swift by apple"
+}
+
+for (const key in obj){
+    // console.log(key); //for keys
+    console.log(obj[key]); //for Values
+    
+}
